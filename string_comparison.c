f1262a7a4d5bf92f1954009bf713db9b4c04805f
Devyansh Kr2005 <144312351+Devyanshkumarshah@users.noreply.github.com>
2026-08-18 21:12:27 +0530

    Create string_comparison.c

---
+++ string_comparison.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+  char a[20] = "Devyansh";
+  char b[20] = "Devyansh";
+
+  for(int i = 0; a[i] == b[i]; i++)
+    {
+      if(a[i] - b[i] == 0])
+      {
+        printf("String is equal");
+      }
+
+      else
+      {
+        printf("String are not equal");
+      }
+    }
+}
